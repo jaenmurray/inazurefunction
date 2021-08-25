9e52e3c4a23941097d6aa85b8ce8d3cde650808f
jaenmurray <jaencarlosmontanomurray@gmail.com>
2021-08-25 18:42:48 -0500

    Add Employee Entity

---
+++ inazurefunction.Common/Models/EmployeeReg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace inazurefunction.Common.Models
+{
+    public class EmployeeReg
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateReg { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ inazurefunction.Functions/Entities/EmployeeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inazurefunction.Functions.Entities
+{
+    public class EmployeeEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateReg { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
